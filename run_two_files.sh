dda270f5bd9d236258e7c39e0d12e93581342061
Rafed Muhammad Yasir <rafed123@gmail.com>
2025-03-10 23:08:15 -0700

    Redirect to output

--- run_two_files.sh
+++ run_two_files.sh
@@ -6,11 +6,13 @@ technique=$1  # bluir, brtracer, buglocator, vsm
 
 
 for proj in "${projects[@]}"; do
-    command="python two_files_analyzer.py ${proj} ${technique}/${proj}-baseline-average-precision.csv ${technique}/${proj}-clean-average-precision.csv"
+    mkdir -p result-${technique}
+    
+    command="python two_files_analyzer.py ${proj} ${technique}/${proj}-baseline-average-precision.csv ${technique}/${proj}-clean-average-precision.csv > result-${technique}/${proj}-average-precision.txt" 
     echo "Running ${proj} average-precision: $command"
     eval "$command"
 
-    command="python two_files_analyzer.py ${proj} ${technique}/${proj}-baseline-reciprocal-rank.csv ${technique}/${proj}-clean-reciprocal-rank.csv"
+    command="python two_files_analyzer.py ${proj} ${technique}/${proj}-baseline-reciprocal-rank.csv ${technique}/${proj}-clean-reciprocal-rank.csv > result-${technique}/${proj}-reciprocal-rank.txt"
     echo "Running ${proj} reciprocal-rank: $command"
     eval "$command"
 done
