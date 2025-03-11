20e806bd55ffe6a53a2d67588df13972b8b6aca9
Rafed Muhammad Yasir <rafed123@gmail.com>
2025-03-10 23:00:11 -0700

    Fix params

--- run_two_files.sh
+++ run_two_files.sh
@@ -6,11 +6,11 @@ technique=$1  # bluir, brtracer, buglocator, vsm
 
 
 for proj in "${projects[@]}"; do
-    command="python two_files_analyzer.py ${technique}/${proj}-baseline-average-precision.csv ${technique}/${proj}-clean-average-precision.csv"
+    command="python two_files_analyzer.py ${proj} ${technique}/${proj}-baseline-average-precision.csv ${technique}/${proj}-clean-average-precision.csv"
     echo "Running ${proj} average-precision: $command"
     eval "$command"
 
-    command="python two_files_analyzer.py ${technique}/${proj}-baseline-reciprocal-rank.csv ${technique}/${proj}-clean-reciprocal-rank.csv"
+    command="python two_files_analyzer.py ${proj} ${technique}/${proj}-baseline-reciprocal-rank.csv ${technique}/${proj}-clean-reciprocal-rank.csv"
     echo "Running ${proj} reciprocal-rank: $command"
     eval "$command"
 done
