ad60885c7a53401909ebb0c82f91f1154c15c9b0
Rafed Muhammad Yasir <rafed123@gmail.com>
2025-03-10 22:43:03 -0700

    Fix vsm script

--- vsm/metric_calculator.py
+++ vsm/metric_calculator.py
@@ -71,8 +71,9 @@ def calculate_mean_average_precision_at_k(data):
 
 
 project = sys.argv[1]
-typ = sys.argv[2]     
-with open(project+'/results.json', 'r') as file:
+result_directory = sys.argv[2]
+typ = sys.argv[3]     
+with open(result_directory +'/results.json', 'r') as file:
     data = json.load(file)
 
 calculate_mean_reciprocal_rank_at_k(project, data, typ)
--- vsm/run.sh
+++ vsm/run.sh
@@ -4,7 +4,7 @@ projects=("aspectj" "birt" "eclipse" "jdt" "swt" "tomcat")
 type=$1  # baseline, clean
 
 for proj in "${projects[@]}"; do
-  command="python metric_calculator.py ${proj} ../../FL-VSM/results-${type}/${proj} ../../FL-VSM/dataset/${proj}-updated-data.xml ${type}"
+  command="python metric_calculator.py ${proj} ../../FL-VSM/results-${type}/${proj} ${type}"
   echo "Running ${proj} ${type}: $command"
   eval "$command"
 done
